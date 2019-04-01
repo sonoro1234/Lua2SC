600e687242629be7e463ae05f1a18c60a10b97c1
sonoro1234 <sonoro@telefonica.net>
2019-04-01 18:50:33 +0200

    PitchTracker.lua add t_clear

--- lua2SC/lua/ugendefs/PitchTracker.lua
+++ lua2SC/lua/ugendefs/PitchTracker.lua
@@ -9,7 +9,7 @@
 -- t_clear : zeroes analysis buffer when is not 0
 -- returns : estimated frequency, estimated clarity (0 to 1 on kind acr and nsdf), R0 (power of signal)
 PitchTracker=MultiOutUGen:new{name="PitchTracker"}
-function PitchTracker.kr(inp,size,hop,dfreq,thresh,kind,useef,bufnum)
+function PitchTracker.kr(inp,size,hop,dfreq,thresh,kind,useef,bufnum,t_clear)
 	size = size or 1024;hop = hop or 0.5;inp = inp or 0;bufnum = bufnum or -1;dfreq= dfreq or 440;thresh = thresh or 0.85;kind = kind or 0;useef = useef or 0;t_clear = t_clear or 0
 	return PitchTracker:MultiNew{1,3,inp,size,hop,dfreq,thresh,kind,useef,bufnum,t_clear}
 end
