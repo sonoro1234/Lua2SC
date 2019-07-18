c2a43828d7890ff577dfb402d55cc48f0b459638
sonoro1234 <sonoro@telefonica.net>
2019-07-18 11:52:09 +0200

    IIRf: allow multiexpand

--- lua2SC/lua/ugendefs/IIRf.lua
+++ lua2SC/lua/ugendefs/IIRf.lua
@@ -1,5 +1,11 @@
 IIRf = UGen:new{name="IIRf"}
 function IIRf.ar(inp, kB,kA)
 	inp = inp or 0;
+	if isMultiExpandable(kB[1]) then
+		kB = flop(kB)
+	end
+	if isMultiExpandable(kB[1]) then
+		kA = flop(kA)
+	end
 	return IIRf:MultiNew(concatTables({2,inp, #kB,#kA},kB,kA))
 end
