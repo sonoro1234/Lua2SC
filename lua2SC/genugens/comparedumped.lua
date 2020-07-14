daffb4f44f3d51de549215d7178e8f2806c6b8ec
Victor Bombi <sonoroster@gmail.com>
2020-07-14 17:37:36 +0200

    tool for comparing dumped ugens functions also

--- lua2SC/genugens/comparedumped.lua
+++ lua2SC/genugens/comparedumped.lua
@@ -2,21 +2,21 @@ local function DefineU()
 	UGen={ derived={},names={}}
 	function UGen:new(o)
 		o = o or {}
-		table.insert(self.derived,o)
+		self.derived[o.name] = o
 		self.names[o.name] = 1 + (self.names[o.name] or 0)
 		return o
 	end
 	MultiOutUGen={ derived={},names={}}
 	function MultiOutUGen:new(o)
 		o = o or {}
-		table.insert(self.derived,o)
+		self.derived[o.name] = o
 		self.names[o.name] = 1 + (self.names[o.name] or 0)
 		return o
 	end
 	Out={ derived={},names={}}
 	function Out:new(o)
 		o = o or {}
-		table.insert(self.derived,o)
+		self.derived[o.name] = o
 		self.names[o.name] = 1 + (self.names[o.name] or 0)
 		return o
 	end
@@ -26,13 +26,29 @@ local function Compare(A,B)
 	print"----------A not in B"
 	for name,v in pairs(A.names) do
 		if not B.names[name] then
-			print(name , "in A not in B")
+			print(name )
+		else
+			local o1 = A.derived[name]
+			local o2 = B.derived[name]
+			for k,v1 in pairs(o1) do
+				if not o2[k] then
+					print(name,k)
+				end
+			end
 		end
 	end
 	print"----------B not in A"
 	for name,v in pairs(B.names) do
 		if not A.names[name] then
-			print(name , "in A not in B")
+			print(name )
+		else
+			local o1 = B.derived[name]
+			local o2 = A.derived[name]
+			for k,v1 in pairs(o1) do
+				if not o2[k] then
+					print(name,k)
+				end
+			end
 		end
 	end
 end
